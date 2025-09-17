7e6af19b80315eefec6ada8ab4b2946cc1fe2719
Hsin-Wen Chang <bow1226@gmail.com>
2025-09-18 07:33:22 +0800

    feat: Enhance DB connection with environment-based SSL and ping

--- starter/utils/db.go
+++ starter/utils/db.go
@@ -10,21 +10,36 @@ import (
 
 // ConnectDB initializes the database connection
 func ConnectDB() (*gorm.DB, error) {
-    dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    dbName := os.Getenv("DB_NAME")
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
+	env := os.Getenv("APP_ENV")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 
-    dsn := fmt.Sprintf(
-        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
-        dbHost, dbUser, dbPassword, dbName, dbPort,
-    )
+	sslMode := "disable"
+	if env == "production" {
+		sslMode = "require"
+	}
 
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        return nil, err
-    }
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
+		dbHost, dbUser, dbPassword, dbName, dbPort, sslMode,
+	)
 
-    return db, nil
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, fmt.Errorf("database ping failed: %w", err)
+	}
+
+	return db, nil
 }
