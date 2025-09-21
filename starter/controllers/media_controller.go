229e363eda4f046b09a2296c59b89b59cc38553a
Hsin-Wen Chang <bow1226@gmail.com>
2025-09-21 09:36:12 +0800

    feat: Implement CRUD operations for media controller

--- starter/controllers/media_controller.go
+++ starter/controllers/media_controller.go
@@ -1,29 +1,85 @@
 package controllers
 
 import (
-    "cms-backend/models"
-    "cms-backend/utils"
-    "net/http"
-    "strconv"
+	"cms-backend/models"
+	"cms-backend/utils"
+	"net/http"
+	"strconv"
 
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetMedia(c *gin.Context) {
-    // TODO: Implement logic to retrieve all media
+	db := c.MustGet("db").(*gorm.DB)
+	var media []models.Media
+	if err := db.Find(&media).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, utils.HTTPError{Code: 500, Message: err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, media)
 }
 
 func GetMediaByID(c *gin.Context) {
-    db := c.MustGet("db").(*gorm.DB)
-    // TODO: Implement logic to retrieve media by ID
+	db := c.MustGet("db").(*gorm.DB)
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, utils.HTTPError{Code: 400, Message: "Invalid media ID"})
+		return
+	}
+	var media models.Media
+	if err := db.First(&media, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, utils.HTTPError{Code: 404, Message: "Media not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, utils.HTTPError{Code: 500, Message: err.Error()})
+		}
+		return
+	}
+	c.JSON(http.StatusOK, media)
 }
 
 func CreateMedia(c *gin.Context) {
-    // TODO: Implement logic to create new media
+	db := c.MustGet("db").(*gorm.DB)
+	var input models.Media
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, utils.HTTPError{Code: 400, Message: err.Error()})
+		return
+	}
+	tx := db.Begin()
+	if err := tx.Create(&input).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, utils.HTTPError{Code: 500, Message: err.Error()})
+		return
+	}
+	tx.Commit()
+	c.JSON(http.StatusCreated, input)
 }
 
 func DeleteMedia(c *gin.Context) {
-    // TODO: Implement logic to delete media
+	db := c.MustGet("db").(*gorm.DB)
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, utils.HTTPError{Code: 400, Message: "Invalid media ID"})
+		return
+	}
+	var media models.Media
+	if err := db.First(&media, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, utils.HTTPError{Code: 404, Message: "Media not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, utils.HTTPError{Code: 500, Message: err.Error()})
+		}
+		return
+	}
+	tx := db.Begin()
+	if err := tx.Delete(&media).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, utils.HTTPError{Code: 500, Message: err.Error()})
+		return
+	}
+	tx.Commit()
+	c.JSON(http.StatusOK, utils.MessageResponse{Message: "Media deleted"})
 }
-
